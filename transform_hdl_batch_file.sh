5f19bcb082f141bb5e209024728a56aedbb09617
Adam Jazairi <adamjazairi@gmail.com>
2017-12-12 15:36:55 -0500

    Finish transform script

--- transform_hdl_batch_file.sh
+++ transform_hdl_batch_file.sh
@@ -2,4 +2,6 @@
 
 # This script creates a handle batch file that redirects DigiTool links to BCLSCO.
 
-sed -i '' 's/https:\/\/.*=\([0-9]\{6\}\).*$/https:\/\/bclsco.bc.edu\/catalog\/oai:dcollections\.bc\.edu:\1/g' 'hdl_batch_file-working.txt' 
+sed 's/https:\/\/.*=\([0-9]\{6\}\).*$/https:\/\/bclsco.bc.edu\/catalog\/oai:dcollections\.bc\.edu:\1/g' hdl_batch_file-working.txt > sed.out
+sed -i '' 's/https:\/\/.*=\([0-9]\{5\}\).*$/https:\/\/bclsco.bc.edu\/catalog\/oai:dcollections\.bc\.edu:\1/g' sed.out
+mv sed.out hdl_batch_file-working.txt
